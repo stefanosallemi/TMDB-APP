b03e5cbb5fbc6e85b89ce4b3f520bf0c39491ad3
stefanosallemi <stefano.sallemi03@gmail.com>
2023-04-17 21:50:03 +0200

    Updates

---
+++ precache-manifest.2257bdc1b8a7b2f74db4950cf51918ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9e73ad754ce8469bc8a",
+    "url": "/TMDB-APP/static/css/main.543076bc.chunk.css"
+  },
+  {
+    "revision": "f9e73ad754ce8469bc8a",
+    "url": "/TMDB-APP/static/js/main.f9e73ad7.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/TMDB-APP/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "7b775089e4844a7dd881",
+    "url": "/TMDB-APP/static/css/2.d184af2a.chunk.css"
+  },
+  {
+    "revision": "7b775089e4844a7dd881",
+    "url": "/TMDB-APP/static/js/2.7b775089.chunk.js"
+  },
+  {
+    "revision": "a65f544dd9c797f62338",
+    "url": "/TMDB-APP/static/js/runtime~main.a65f544d.js"
+  },
+  {
+    "revision": "f13571dfc18e1efaec56947f44de6e9a",
+    "url": "/TMDB-APP/index.html"
+  }
+];
