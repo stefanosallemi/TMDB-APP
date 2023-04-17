fa1c4b6ac5eb5506cd6f30373bf6988ccff8463d
stefanosallemi <stefano.sallemi03@gmail.com>
2023-04-17 22:28:32 +0200

    upt

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,27 +11,31 @@ interface Props {
 export const Card: React.FC<Props> = ({ id, title, poster_path, overview, vote_average }) => {
     if (overview === "") {
         overview = "Nessuna descrizione";
-      }
-    
-      const shortOverview = overview.slice(0, 80) + "...";
-    
+    }
+    const shortOverview = overview.slice(0, 70) + "...";
+
     return (
-        <div className="card d-inline-flex" style={{ width: "16rem", paddingRight: "2rem", borderRadius: "2rem", margin: "1rem" }} key={id}>
-            <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-                alt={title} className="card-img-top" style={{ margin: "1rem", borderRadius: "1.5rem" }} />
+        <div className="card" style={{ width: "17rem", borderRadius: "2rem", paddingTop: "0.7rem", margin: "1rem" }} key={id}>
+    {poster_path ? (
+        <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="card-img-top" style={{ borderRadius: "1.5rem" }} />
+    ) : (
+        <img src="/Card/default.webp" alt="Copertina non disponibile" className="card-img-top" style={{ borderRadius: "1.5rem" }} />
+    )}
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">
                     {shortOverview}
                 </p>
-                <a href="#" className="btn btn-primary rounded-pill">
-                    Mostra
-                </a>
-                <a href="#" className="btn btn-success rounded-pill">
-                {parseInt(vote_average)}/10
-                </a>
+                <div className='row justify-content-between' style={{bottom: "0"}}>
+                    <a href="#" className="btn btn-primary rounded-pill col">
+                        Mostra
+                    </a>
+                    <a href="#" className="btn btn-success rounded-pill col">
+                        {parseInt(vote_average)}/10
+                    </a>
+                </div>
             </div>
 
-    </div>
+        </div>
     )
 }
